1e9af6b33712e07689bea672eb76e0a39b1b19e5
xuguohui <yefengmengluo@163.com>
2016-02-15 15:23:37 +0800

    1

--- src/main/java/personal/controller/VoteAdvertisingController.java
+++ src/main/java/personal/controller/VoteAdvertisingController.java
@@ -1,12 +1,19 @@
 package personal.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import personal.entity.VoteAdvertising;
 import personal.service.VoteAdvertisingService;
+import personal.util.Page;
 
 @Controller
 @RequestMapping("advertising")
@@ -14,20 +21,29 @@ public class VoteAdvertisingController {
 	
 	@Autowired
 	private VoteAdvertisingService service;
-	
-//	@RequestMapping(value = "/list")
-//	public String getList(){
-//		service.getList();
-//		return "list";
-//	}
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/list")
+	public String getList(HttpServletRequest request){
+		Page<VoteAdvertising> page = new Page<VoteAdvertising>();
+		page.setPageNo(2);
+		Page<VoteAdvertising> list = service.getList(page);
+		request.setAttribute("list", list);
+		return "list";
+	}
 
 	//使用@ResponseBody注解，返回json格式
-//	@RequestMapping("/list")
-//	@ResponseBody
-//	public List<VoteAdvertising> getList(){
-//		List<VoteAdvertising> list = service.getList();
-//		return list;
-//	}
+	@RequestMapping("/list")
+	@ResponseBody
+	public List<VoteAdvertising> getList2(){
+		Page<VoteAdvertising> page = new Page<VoteAdvertising>();
+		page.setPageNo(2);
+		List<VoteAdvertising> list = service.getList(page).getResults();
+		return list;
+	}
 
 	//rest 风格的请求方式
 	@RequestMapping(value = "/getById/{id}")
